7b06f63cb5965f1c12b66f0d9a7839fdf736b5e9
jyhwng <jyhwng@gmail.com>
2019-11-14 14:59:47 +0900

    Add State, EventHandler from ggez

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use ggez::{Context, ContextBuilder, GameResult};
+use ggez::event::{self, EventHandler};
+use ggez::graphics;
+
+fn main() -> GameResult {
+    let (mut ctx, mut event_loop) =
+       ContextBuilder::new("Bubble Puzzle", "jyhwng")
+           .build()
+           .unwrap();
+    let ctx = &mut ctx;
+    
+    let mut state = State::new(ctx);
+
+    event::run(ctx, &mut event_loop, &mut state)
+}
+
+#[derive(Debug)]
+struct State {} // Type declaration
+
+impl State {    // Implementation
+    fn new(ctx: &mut Context) -> Self {
+        State {}    // Expression oriented (<-> Statement) Always return the last 
+    }
+}
+
+impl EventHandler for State {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
 }
